d0a9b2a2a9447bb5f66f66e2de5bf53ba64a5342
Игорь Сариев <igor.sariev@yandex.ru>
2022-12-14 01:08:09 +0300

    Добавьте файлы проекта.

---
+++ Model/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestSaber.Model
+{
+    public class Number
+    {
+        public int Value { get; private set; }
+        public int DivValue { get; set; }
+        public int Sign { get; private set; }
+        public int Multiplier
+        {
+            get { return Value < 0 ? -1 : 1; }
+        }
+        public int ModValue
+        {
+            get { return (DivValue % 2) * Multiplier; }
+        }
+
+        public Number ()
+        {
+            Read();
+            DivValue = Value;
+            Sign = Value < 0 ? 1 : 0;
+        }
+        public void Read()
+        {
+            Console.WriteLine("Введите целое число");
+            Value = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Text;
+using TestSaber.Model;
+
+namespace TestSaber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                var num = new Number();
+                var numBit = "";
+                var result =  num.Sign == 1 ?  "11111" : "00000";
+                // num.Read();
+                do
+                {
+                    var el = (num.ModValue - num.Sign);
+
+                    numBit += (el* num.Multiplier).ToString();
+                    num.DivValue /= 2;
+                }
+                while (num.DivValue != 0);
+
+                for (int i = numBit.Length-1; i >= 0;  i--) {
+                    result += numBit[i];
+                }
+                //num.DivValue = num.DivValue / 2;
+                //numBit.Append(num.ModValue.ToString());
+                //while (num.DivValue != 0) 
+                //{
+                //    numBit.Append(num.ModValue.ToString());
+                //    num.DivValue = num.DivValue / 2;
+                //}
+                Console.WriteLine(result);
+               // Console.ReadLine();
+            }
+        }
+    }
+}
