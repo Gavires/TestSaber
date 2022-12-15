f0a8bd4a7f208ac6dd3059cc95f580f5a9730a89
Сариев Игорь <ISariev@softrust.ru>
2022-12-15 14:47:20 +0300

    Промежуточный коммит

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using TestSaber.Model;
+using TestSaber.Task.Task1;
 
 namespace TestSaber
 {
@@ -7,29 +8,8 @@ namespace TestSaber
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                var numBit = "";
-                var result = "";
-                var num = new Number();
-                do
-                {
-                    var el = (num.ModValue - num.Sign);
-
-                    numBit += (el * num.Multiplier).ToString();
-                    num.DivValue /= 2;
-                }
-                while (num.DivValue != 0);
-
-                for (int i = numBit.Length - 1; i >= 0; i--)
-                {
-                    result += numBit[i];
-                }
-                var res = num.Sign == 1 ? num.NegativeNumber(result) : result;
-                var flag = Convert.ToInt32(res, 2) == num.Value ? "равно" : "неравно";
-                Console.WriteLine(res);
-                Console.WriteLine($"Проверка: число {res} в десятичной системе {Convert.ToInt32(res, 2)} {flag} исходному числу {num.Value}");
-            }
+            var task1 = new NumberToBin();
+            task1.Task1();
         }
     }
 }
---
+++ Task/Task1/NumberToBin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestSaber.Model;
+
+namespace TestSaber.Task.Task1
+{
+    public class NumberToBin
+    {
+        /// <summary>
+        /// Задача 1
+        /// </summary>
+        /// <returns>Перевод в двоичную систему  введенного с клавиатуры десятичного знакового числа</returns>
+        public void Task1()
+        {
+            while (true)
+            {
+                var numBit = "";
+                var result = "";
+                var num = new Number();
+                do
+                {
+                    numBit += ((num.ModValue - num.Sign) * num.Multiplier).ToString();
+                    num.DivValue /= 2;
+                }
+                while (num.DivValue != 0);
+
+                for (int i = numBit.Length - 1; i >= 0; i--)
+                {
+                    result += numBit[i];
+                }
+                var res = num.Sign == 1 ? num.NegativeNumber(result) : result;
+                var flag = Convert.ToInt32(res, 2) == num.Value ? "равно" : "неравно";
+                Console.WriteLine(res);
+                Console.WriteLine($"Проверка: число {res} в десятичной системе {Convert.ToInt32(res, 2)} {flag} исходному числу {num.Value}");
+            }
+        }
+    }
+}
