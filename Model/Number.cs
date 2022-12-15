58f36c085b7b0de336d5b39c9b89bec3a5d99f05
Сариев Игорь <ISariev@softrust.ru>
2022-12-15 14:37:08 +0300

    Итоговый коммит по 1 заданию

--- Model/Number.cs
+++ Model/Number.cs
@@ -4,7 +4,8 @@ namespace TestSaber.Model
 {
     public class Number
     {
-        public int Value { get; private set; }
+        public int Value { get; set; }
+        public int ValueВischarge { get; private set; }
         public int DivValue { get; set; }
         public int Sign { get; private set; }
         public int Multiplier
@@ -16,16 +17,71 @@ namespace TestSaber.Model
             get { return (DivValue % 2) * Multiplier; }
         }
 
-        public Number ()
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <returns>Создает новый объект Number из введенного с клавиатуры числа</returns>
+        public Number()
         {
             Read();
             DivValue = Value;
             Sign = Value < 0 ? 1 : 0;
         }
+
+        /// <summary>
+        /// Ввод числа с клавиатуры
+        /// </summary> Вводит число с клавиатуры и записывает в объект Number
+        /// <returns></returns>
         public void Read()
         {
             Console.WriteLine("Введите целое число");
-            Value = int.Parse(Console.ReadLine());
+            try
+            {
+                Value = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Определение разрядности для отрицательного числа (с условием типа int)
+        /// </summary>
+        /// <param name="str">двоичное представление введенного числа</param>
+        /// <returns>Возвращает дополнительный код для отрицательного числа</returns>
+        private string Discharge(string str)
+        {
+            var result = "";
+            for (var i = 0; i < 32 - str.Length; i++)
+                result += 1;
+            return result;
+        }
+
+        /// <summary>
+        /// Добавление единицы к двоичному представлению числа
+        /// </summary>
+        /// <param name="str">двоичное представление введенного числа</param>
+        /// <returns>Возвращает двоичное представление числа</returns>
+        public string NegativeNumber(string str)
+        {
+            var i = str.Length - 1;
+            var result = Discharge(str);
+            while (str[i] != '0' && i > 0)
+            {
+                i--;
+            }
+            for (var j = 0; j < i; j++)
+            {
+                result += str[j];
+            }
+            result += 1;
+            for (var j = i; j < str.Length - 1; j++)
+            {
+                result += 0;
+            }
+            return result;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Text;
+﻿using System;
 using TestSaber.Model;
 
 namespace TestSaber
@@ -9,12 +7,13 @@ namespace TestSaber
     {
         static void Main(string[] args)
         {
-            while (true) {
-                var num = new Number();
+            while (true)
+            {
                 var numBit = "";
-                var result = num.Sign == 1 ? "11111" : "";
-                // num.Read();
-                do {
+                var result = "";
+                var num = new Number();
+                do
+                {
                     var el = (num.ModValue - num.Sign);
 
                     numBit += (el * num.Multiplier).ToString();
@@ -22,32 +21,14 @@ namespace TestSaber
                 }
                 while (num.DivValue != 0);
 
-                for (int i = numBit.Length - 1; i >= 0; i--) {
+                for (int i = numBit.Length - 1; i >= 0; i--)
+                {
                     result += numBit[i];
                 }
-
-                var res = num.Sign == 1 ? otr(result) : result;
+                var res = num.Sign == 1 ? num.NegativeNumber(result) : result;
+                var flag = Convert.ToInt32(res, 2) == num.Value ? "равно" : "неравно";
                 Console.WriteLine(res);
-                // Console.ReadLine();
-                Console.WriteLine(Convert.ToInt16(res, 2));
-            }
-
-            string otr(string str)
-            {
-                var i = str.Length - 1;
-                var result = "";
-                while (str[i] != '0' && i > 0) {
-                    i--;
-                }
-                for (var j = 0; j < i; j++) {
-                    result += str[j];
-                }
-                result += 1;
-                for (var j = i; j < str.Length - 1; j++) {
-                    result += 0;
-                }
-                return result;
-            
+                Console.WriteLine($"Проверка: число {res} в десятичной системе {Convert.ToInt32(res, 2)} {flag} исходному числу {num.Value}");
             }
         }
     }
